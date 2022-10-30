93b64044cb631f0a4d70b8224ae179a575f7f2de
hooman mousavi <hoomanmousavi77@gmail.com>
2022-10-30 20:52:21 +0330

    pie chart added

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,11 @@ import Sidebar from "./components/Sidebar/Sidebar.js";
 import Dropdown from "./components/Dropdown/Dropdown.js";
 import PureCounter from "@srexi/purecounterjs";
 import Chart from "./components/Chart/Chart.js";
+import PieChart from "./components/Chart/pieChart.js";
 ///////////////////////////////////////////
 window.Alpine = Alpine
 Alpine.data('sidebar',Sidebar)
 Alpine.data('dropdown',Dropdown)
 Alpine.start()
 const pure = new PureCounter();
-am4core.ready(Chart())
+am4core.ready(Chart(),PieChart())
